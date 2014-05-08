ec164e713a75f1d95688921796d760d55cd183a6
Jorge Izquierdo Ciges <jorizci@gmail.com>
2014-05-08 12:20:00 +0200

    Code comment.

--- src/main/java/com/biit/jointjs/diagram/builder/server/DiagramBuilder.java
+++ src/main/java/com/biit/jointjs/diagram/builder/server/DiagramBuilder.java
@@ -24,35 +24,39 @@ import com.biit.jointjs.diagram.builder.client.DiagramBuilderServerRpc;
 import com.vaadin.annotations.JavaScript;
 import com.vaadin.ui.AbstractComponent;
 
-@JavaScript({"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/dist/joint.js",
-	"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/dist/joint.shapes.devs.js",
-	"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/dist/joint.shapes.fsa.js",
-	"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/dist/joint.shapes.pn.js",
-	"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/dist/joint.shapes.erd.js",
-	"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/dist/joint.shapes.uml.js",
-	"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/dist/joint.shapes.org.js",
-	"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/dist/joint.ui.halo.js",
-	"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/dist/joint.dia.freeTransform.js",
-	"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/dist/joint.ui.freeTransform.js",
-	"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/dist/joint.ui.inspector.js",
-	"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/dist/joint.ui.selectionView.js",
-	"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/dist/joint.ui.clipboard.js",
-	"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/dist/joint.ui.stencil.js",
-	"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/dist/joint.ui.paperScroller.js",
-	"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/dist/joint.ui.tooltip.js",
-	"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/dist/joint.format.svg.js",
-	"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/dist/joint.format.raster.js",
-	"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/dist/joint.format.print.js",
-	"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/dist/joint.dia.command.js",
-	"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/dist/joint.dia.validator.js",
-	"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/dist/joint.layout.ForceDirected.js",
-	"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/dist/joint.layout.GridLayout.js",
-	"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/dist/joint.layout.DirectedGraph.js",
-	"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/lib/keyboard.js",
-	"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/src/inspector.js",
-	"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/src/dataTypes.js",
-	"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/src/main.js",
-	})
+/**
+ * Diagram builder component based on JointJs javascript library and RapidJs
+ * framework. Commercial license only.
+ * 
+ */
+@JavaScript({ "vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/dist/joint.js",
+		"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/dist/joint.shapes.devs.js",
+		"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/dist/joint.shapes.fsa.js",
+		"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/dist/joint.shapes.pn.js",
+		"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/dist/joint.shapes.erd.js",
+		"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/dist/joint.shapes.uml.js",
+		"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/dist/joint.shapes.org.js",
+		"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/dist/joint.ui.halo.js",
+		"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/dist/joint.dia.freeTransform.js",
+		"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/dist/joint.ui.freeTransform.js",
+		"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/dist/joint.ui.inspector.js",
+		"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/dist/joint.ui.selectionView.js",
+		"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/dist/joint.ui.clipboard.js",
+		"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/dist/joint.ui.stencil.js",
+		"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/dist/joint.ui.paperScroller.js",
+		"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/dist/joint.ui.tooltip.js",
+		"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/dist/joint.format.svg.js",
+		"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/dist/joint.format.raster.js",
+		"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/dist/joint.format.print.js",
+		"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/dist/joint.dia.command.js",
+		"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/dist/joint.dia.validator.js",
+		"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/dist/joint.layout.ForceDirected.js",
+		"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/dist/joint.layout.GridLayout.js",
+		"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/dist/joint.layout.DirectedGraph.js",
+		"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/lib/keyboard.js",
+		"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/src/inspector.js",
+		"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/src/dataTypes.js",
+		"vaadin://widgetsets/com.biit.jointjs.diagram.builder.JointJsWidgetBuilder/src/main.js", })
 public class DiagramBuilder extends AbstractComponent {
 	private static final long serialVersionUID = -6846503871084162514L;
 	private DiagramBuilderServerRpc serverRpc;
@@ -77,61 +81,61 @@ public class DiagramBuilder extends AbstractComponent {
 		return getRpcProxy(DiagramBuilderClientRpc.class);
 	}
 
-	public void undo(){
+	public void undo() {
 		getClientRpc().undo();
 	}
-	
-	public void redo(){
+
+	public void redo() {
 		getClientRpc().redo();
 	}
-	
-	public void clear(){
+
+	public void clear() {
 		getClientRpc().clear();
 	}
-	
-	public void openAsSvg(){
+
+	public void openAsSvg() {
 		getClientRpc().openAsSvg();
 	}
-	
-	public void openAsPng(){
+
+	public void openAsPng() {
 		getClientRpc().openAsPng();
 	}
-	
-	public void zoomIn(){
+
+	public void zoomIn() {
 		getClientRpc().zoomIn();
 	}
-	
-	public void zoomOut(){
+
+	public void zoomOut() {
 		getClientRpc().zoomOut();
 	}
-	
-	public void print(){
+
+	public void print() {
 		getClientRpc().print();
 	}
-	
-	public void toFront(){
+
+	public void toFront() {
 		getClientRpc().toFront();
 	}
-	
-	public void toBack(){
+
+	public void toBack() {
 		getClientRpc().toBack();
 	}
-	
-	public void toJson(DiagramBuilderJsonGenerationListener listener){
+
+	public void toJson(DiagramBuilderJsonGenerationListener listener) {
 		listeners.add(listener);
 		getClientRpc().toJsonQuery();
 	}
-	
-	public void fromJson(String jsonString){
+
+	public void fromJson(String jsonString) {
 		getRpcProxy(DiagramBuilderClientRpc.class).fromJson(jsonString);
 	}
-	
+
 	public interface DiagramBuilderJsonGenerationListener {
-		public void generatedJsonString(String jsonString);		
+		public void generatedJsonString(String jsonString);
 	}
-	
-	private synchronized void fireAndCleanListeners(String jsonString){
-		for(DiagramBuilderJsonGenerationListener listener: listeners){
+
+	private synchronized void fireAndCleanListeners(String jsonString) {
+		for (DiagramBuilderJsonGenerationListener listener : listeners) {
 			listener.generatedJsonString(jsonString);
 		}
 		listeners.clear();
